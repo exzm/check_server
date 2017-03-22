46c6057473e9c3c0f0da2e1e8ef592d6308b8b31
Voinkov <voinkov.andrey@yandex.ru>
2017-03-22 12:47:16 +0500

    Add config.php

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+
+define('SERVER_NAME', 'SERVER_NAME');
+define('EMAIL', 'email@email.com');
+define('MIN_FREE_DISK', 50);
+define('MAX_LOAD_AVERAGE', 50);
--- run.php
+++ run.php
@@ -1,12 +1,7 @@
 <?php
 
 require_once "CheckServer.class.php";
-
-define('SERVER_NAME', 'SERVER_NAME');
-define('EMAIL', 'email@email.com');
-define('MIN_FREE_DISK', 50);
-define('MAX_LOAD_AVERAGE', 50);
-
+require_once "config.php";
 
 $check = new CheckServer();
 $check->setLoadAverage(MAX_LOAD_AVERAGE);
